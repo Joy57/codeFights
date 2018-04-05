eda1816e022d92f839fc433c679066efdb3b9672
Joy <abedinjoynal2@gmail.com>
2018-04-04 22:02:22 -0400

    prob9

---
+++ arcade/prob9/longString.js
@@ -0,0 +1,8 @@
+function longString(inputArray){
+    // var len= [inputArray.length];
+    for (var i = 0; i < inputArray.length; i++){
+        console.log(inputArray.length[0]);
+    }
+}
+
+var inputArray = ["aba", "aa", "ad", "vcd", "aba"];
